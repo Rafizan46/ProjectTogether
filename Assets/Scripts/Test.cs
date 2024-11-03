ea8a91f5865255f4198c95831c806634407f3757
Arlow5761 <arlow5761@gmail.com>
2024-11-03 12:11:48 +0700

    feat: change log message

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hellooow");
+        Debug.Log("Hello everynyan!");
     }
 
     // Update is called once per frame
