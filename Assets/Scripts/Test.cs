1747b890dc0063efe0b83bf5be189485fb93db09
Rafizan <163702740+Rafizan46@users.noreply.github.com>
2024-11-03 12:12:13 +0700

    Update Test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hellooow");
+        Debug.Log("Hellooow GIMAPP 24");
     }
 
     // Update is called once per frame
